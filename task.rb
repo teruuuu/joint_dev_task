fc44949d81fa71152b489ab42a97f0316bfc5ec3
teru <titlt.aiw6@gmail.com>
2021-10-13 14:21:14 +0900

    Q10,11回答 to task.rb

--- task.rb
+++ task.rb
@@ -90,6 +90,13 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
+foods.each do |food|
+  if food.include?("うに")
+    puts "好物です"
+  else
+    puts "まぁまぁ好きです"
+  end
+end
 
 end
 
@@ -97,7 +104,13 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+flatten_sports = sports.flatten
+flatten_sports.uniq!
 
+   puts "ユーザーの趣味一覧"
+flatten_sports.each.with_index(1) do |sport, i|
+   puts "No.#{i} #{sport}"
+ end
 end
 
 def q12
